8a47dc614ecaeff9584669853e310c2b94077276
watlix <93144173+watlix@users.noreply.github.com>
2021-11-09 14:41:03 -0500

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+angle = 0
+anglew = 0
+servos.P0.set_range(10, 170)
+
 def on_forever():
-    pass
+    global anglew, angle
+    for index in range(17):
+        anglew = index * 10 + 10
+        servos.P0.set_angle(anglew)
+        basic.pause(100)
+    for index2 in range(33):
+        angle = anglew - index2 * 5
+        servos.P0.set_angle(angle)
+        basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+let angle = 0
 let anglew = 0
 servos.P0.setRange(10, 170)
-basic.forever(function () {
-    for (let index = 0; index <= 16; index++) {
+basic.forever(function on_forever() {
+    
+    for (let index = 0; index < 17; index++) {
         anglew = index * 10 + 10
         servos.P0.setAngle(anglew)
+        basic.pause(100)
+    }
+    for (let index2 = 0; index2 < 33; index2++) {
+        angle = anglew - index2 * 5
+        servos.P0.setAngle(angle)
         basic.pause(100)
     }
 })
